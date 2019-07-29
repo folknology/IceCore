aba159a433b8f21ca5d07c2072eb62b36ae25e9b
folknology <folknology@gmail.com>
2019-07-29 14:33:05 +0100

    Added back the Host ->USB->Uart->Ice40 now that I have tunred the auto echo off!!

--- firmware/myStorm/mystorm.cpp
+++ firmware/myStorm/mystorm.cpp
@@ -271,8 +271,8 @@ static int8_t usbcdc_rxcallback(uint8_t *data, uint32_t *len){
 		// HAL_UART_Transmit(&huart1, data, *len, HAL_UART_TIMEOUT_VALUE);
 		// mode_led_toggle();
 
-		// HAL_UART_Transmit_DMA(&huart1, data, *len);
-		// return USBD_OK;
+		HAL_UART_Transmit_DMA(&huart1, data, *len);
+		return USBD_OK;
 		//if(temp) mode_led_low();
 	}
 
